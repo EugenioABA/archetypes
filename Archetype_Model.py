881210808b80fb0f6f5d9af55126c41612742175
EugenioABA <58227437+EugenioABA@users.noreply.github.com>
2024-05-09 20:58:55 -0500

    Update Archetype_Model.py

--- Archetype_Model.py
+++ Archetype_Model.py
@@ -1,21 +1,3 @@
-# this is the code for the model that predicts the archetype of a tagline.
-#after saving the model, it can be loaded and used to predict the archetype of a new tagline without needing to retrain the model!
-
-# def archetype_prediction(tagline): # tagline is the new tagline that we want to predict the archetype for
-#     import joblib # joblib is used to save and load the model
-#     # Load the saved model
-#     loaded_model = joblib.load("archetype_classifier.pkl") # this should be the exact path where the model is saved, unless it is in the current working directory. 
-#     # Predict the archetype for the new tagline
-#     probabilities = loaded_model.predict_proba([tagline]) # predict the probabilities of each archetype for the new tagline
-#     class_labels = loaded_model.classes_ # get the class labels of the model
-#     predicted_archetype = loaded_model.predict([tagline]) # predict the archetype of the new tagline
-#     probability = round(probabilities[0][list(class_labels).index(predicted_archetype[0])],2)*100
-#      # return the predicted archetype and the probabilities pf the most likely archetype
-#     #if the probability is less than 20%, assigned archetype as "Undefined"
-#     if probability < 20:
-#         return "Undefined", "This text lacks character 100"
-#     else:
-#         return predicted_archetype[0], probability
 def archetype_prediction(tagline):
     import joblib
     loaded_model = joblib.load("archetype_classifier.pkl")
